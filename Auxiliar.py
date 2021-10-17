89979484440e1e6eddfbae1b28e97a74f1391c39
EdsonOli <edso06@gmail.com>
2021-10-17 17:27:00 -0300

    busca

--- Auxiliar.py
+++ Auxiliar.py
@@ -1,3 +1,5 @@
+from No import No
+
 class Auxiliar:
     def detecta_ameacas(self, matriz, linha, coluna):
         contador = 0
@@ -35,3 +37,20 @@ class Auxiliar:
                     contador += 1
                 c += 1
         return (contador - 6)
+
+    def caminhos(self, no):
+        caminho = []
+        while(no != None):
+            caminho.append(no)
+            no = no.pai
+        return caminho
+
+    def expande(self, no, problema):
+        filhos = []
+        possibilidades = problema.acao(no)
+        if (possibilidades != []):
+            for no in range(len(possibilidades)):
+                no_filho = No()
+                no_filho.__init__(possibilidades[no].estado, no, no.custo, no.profundidade + 1)
+                filhos.append(no_filho)
+        return filhos
--- Busca.py
+++ Busca.py
@@ -1,5 +1,18 @@
-import No
+from Auxiliar import Auxiliar
+from No import No
 
 class Busca:
     def busca_em_largura(self, problema, no):
-        
+        borda = []
+        Nos = No()
+        Nos._init_(problema.estado_iniicial, None, 0, 0)
+        borda.insert(0, Nos)
+        while(True):
+            Auxiliar.imprimirMatriz(self, borda[0].estado)
+            if(problema.teste_obsetivo(borda[0])):
+                print("Profundidade total:", bord[0].profundidade)
+                return Auxiliar.caminhos(self,  borda[0])
+            filhos = Auxiliar.expande(self, borda[0], problema)
+            borda.pop(0)
+            for filho in range(filhos):
+                borda.append(filhos[filho])
